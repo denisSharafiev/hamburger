1dc0cb841e3b0d53e1df138f3c8af5d39d5ea1f3
Denis Sharafiev <115945580+denisSharafiev@users.noreply.github.com>
2022-10-27 23:01:06 +0400

    Create hamburger.js

---
+++ hamburger.js
@@ -0,0 +1,15 @@
+function hamburger() {
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu'),
+          closeElem = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    closeElem.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+}
+
+hamburger();
